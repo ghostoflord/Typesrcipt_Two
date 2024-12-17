da41c4d109efc094784fb86c3b7efb45cbd31e54
ghostoflord <ghost2892003zzz@gmail.com>
2024-12-17 13:29:32 +0700

    update login

--- src/pages/client/auth/login.tsx
+++ src/pages/client/auth/login.tsx
@@ -1,8 +1,86 @@
+import { App, Button, Divider, Form, Input } from 'antd';
+import type { FormProps } from 'antd';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './login.scss';
+import { loginAPI } from '@/services/api';
+
+type FieldType = {
+    userName: string;
+    password: string;
+};
+
 const LoginPage = () => {
+    const [isSubmit, setIsSubmit] = useState(false);
+    const { message } = App.useApp();
+    const navigate = useNavigate();
+
+    const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
+        setIsSubmit(true);
+        const { userName, password } = values;
+
+        const res = await loginAPI(userName, password);
+        if (res.data) {
+            //success
+            message.success("Đăng nhập thành công.")
+            navigate("/")
+        } else {
+            //error
+            message.error(res.message)
+        }
+        setIsSubmit(false);
+    };
+
     return (
-        <div>
-            LoginPage
+        <div className="login-page">
+            <main className="main">
+                <div className="container">
+                    <section className="wrapper">
+                        <div className="heading">
+                            <h2 className="text text-large">Đăng Nhập Tài Khoản</h2>
+                            <Divider />
+                        </div>
+                        <Form
+                            name="form-register"
+                            onFinish={onFinish}
+                            autoComplete="off"
+                        >
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }} //whole column
+                                label="Email"
+                                name="userName"
+                                rules={[{ required: true, message: 'Email không được để trống!' }]}
+                            >
+                                <Input />
+                            </Form.Item>
+
+                            <Form.Item<FieldType>
+                                labelCol={{ span: 24 }} //whole column
+                                label="Mật khẩu"
+                                name="password"
+                                rules={[{ required: true, message: 'Mật khẩu không được để trống!' }]}
+                            >
+                                <Input.Password />
+                            </Form.Item>
+
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" loading={isSubmit}>
+                                    Đăng nhập
+                                </Button>
+                            </Form.Item>
+                            <Divider>Or</Divider>
+                            <p className="text text-normal" style={{ textAlign: "center" }}>
+                                Chưa có tài khoản ?
+                                <span>
+                                    <Link to='/register' > Đăng KýKý </Link>
+                                </span>
+                            </p>
+                        </Form>
+                    </section>
+                </div>
+            </main>
         </div>
     )
 }
+
 export default LoginPage;
