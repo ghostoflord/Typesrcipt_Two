e6e7706056b4975995449743a3f96784edfbb362
ghostoflord <ghost2892003zzz@gmail.com>
2024-12-25 21:06:08 +0700

    update

--- src/components/admin/user/table.user.tsx
+++ src/components/admin/user/table.user.tsx
@@ -129,11 +129,10 @@ const TableUser = () => {
                     }
 
                     //default
-                    query += `&sort=-createdAt`;
 
                     if (sort && sort.createdAt) {
                         query += `&sort=${sort.createdAt === "ascend" ? "createdAt" : "-createdAt"}`
-                    }
+                    } else query += `&sort=-createdAt`;
 
                     const res = await getUsersAPI(query);
                     if (res.data) {
