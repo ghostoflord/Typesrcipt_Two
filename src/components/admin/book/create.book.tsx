3143817b99624020bcd0251f9504c8b55cc07a30
ghostoflord <ghost2892003zzz@gmail.com>
2024-12-27 09:58:33 +0700

    update add book fix

--- src/components/admin/book/create.book.tsx
+++ src/components/admin/book/create.book.tsx
@@ -1,3 +1,4 @@
+
 import { useEffect, useState } from 'react';
 import {
     App,
@@ -6,16 +7,20 @@ import {
 } from 'antd';
 import { LoadingOutlined, PlusOutlined } from '@ant-design/icons';
 import type { FormProps } from 'antd';
-import { getCategoryAPI } from '@/services/api';
+import { createBookAPI, getCategoryAPI, uploadFileAPI } from '@/services/api';
 import type { GetProp, UploadFile, UploadProps } from 'antd';
 import { MAX_UPLOAD_IMAGE_SIZE } from '@/services/helper';
 import { UploadChangeParam } from 'antd/es/upload';
+import { UploadRequestOption as RcCustomRequestOptions } from 'rc-upload/lib/interface';
 
 type FileType = Parameters<GetProp<UploadProps, 'beforeUpload'>>[0];
+
+type UserUploadType = "thumbnail" | "slider";
 
 interface IProps {
     openModalCreate: boolean;
     setOpenModalCreate: (v: boolean) => void;
+    refreshTable: () => void;
 }
 
 type FieldType = {
@@ -29,7 +34,7 @@ type FieldType = {
 };
 
 const CreateBook = (props: IProps) => {
-    const { openModalCreate, setOpenModalCreate } = props;
+    const { openModalCreate, setOpenModalCreate, refreshTable } = props;
     const { message, notification } = App.useApp();
     const [form] = Form.useForm();
 
@@ -45,6 +50,9 @@ const CreateBook = (props: IProps) => {
 
     const [previewOpen, setPreviewOpen] = useState<boolean>(false);
     const [previewImage, setPreviewImage] = useState<string>('');
+
+    const [fileListThumbnail, setFileListThumbnail] = useState<UploadFile[]>([]);
+    const [fileListSlider, setFileListSlider] = useState<UploadFile[]>([]);
 
     useEffect(() => {
         const fetchCategory = async () => {
@@ -62,7 +70,34 @@ const CreateBook = (props: IProps) => {
 
     const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
         setIsSubmit(true)
-        console.log(values)
+
+        const { mainText, author, price, quantity, category } = values;
+        const thumbnail = fileListThumbnail?.[0]?.name ?? "";
+        const slider = fileListSlider?.map(item => item.name) ?? [];
+
+        const res = await createBookAPI(
+            mainText,
+            author,
+            price,
+            quantity,
+            category,
+            thumbnail,
+            slider
+        );
+        if (res && res.data) {
+            message.success('Tạo mới book thành công');
+            form.resetFields();
+            setFileListSlider([]);
+            setFileListThumbnail([]);
+            setOpenModalCreate(false);
+            refreshTable();
+        } else {
+            notification.error({
+                message: 'Đã có lỗi xảy ra',
+                description: res.message
+            })
+        }
+
         setIsSubmit(false)
     };
 
@@ -84,7 +119,7 @@ const CreateBook = (props: IProps) => {
         if (!isLt2M) {
             message.error(`Image must smaller than ${MAX_UPLOAD_IMAGE_SIZE}MB!`);
         }
-        return isJpgOrPng && isLt2M;
+        return isJpgOrPng && isLt2M || Upload.LIST_IGNORE;
     };
 
 
@@ -97,7 +132,18 @@ const CreateBook = (props: IProps) => {
         setPreviewOpen(true);
     };
 
-    const handleChange = (info: UploadChangeParam, type: "thumbnail" | "slider") => {
+    const handleRemove = async (file: UploadFile, type: UserUploadType) => {
+        if (type === 'thumbnail') {
+            setFileListThumbnail([])
+        }
+        if (type === 'slider') {
+            const newSlider = fileListSlider.filter(x => x.uid !== file.uid);
+            setFileListSlider(newSlider);
+        }
+    };
+
+
+    const handleChange = (info: UploadChangeParam, type: UserUploadType) => {
         if (info.file.status === 'uploading') {
             type === "slider" ? setLoadingSlider(true) : setLoadingThumbnail(true);
             return;
@@ -110,11 +156,29 @@ const CreateBook = (props: IProps) => {
     };
 
 
-    const handleUploadFile: UploadProps['customRequest'] = ({ file, onSuccess, onError }) => {
-        setTimeout(() => {
+    const handleUploadFile = async (options: RcCustomRequestOptions, type: UserUploadType) => {
+        const { onSuccess } = options;
+        const file = options.file as UploadFile;
+        const res = await uploadFileAPI(file, "book");
+
+        if (res && res.data) {
+            const uploadedFile: any = {
+                uid: file.uid,
+                name: res.data.fileUploaded,
+                status: 'done',
+                url: `${import.meta.env.VITE_BACKEND_URL}/images/book/${res.data.fileUploaded}`
+            }
+            if (type === "thumbnail") {
+                setFileListThumbnail([{ ...uploadedFile }])
+            } else {
+                setFileListSlider((prevState) => [...prevState, { ...uploadedFile }])
+            }
+
             if (onSuccess)
-                onSuccess("ok");
-        }, 1000);
+                onSuccess('ok')
+        } else {
+            message.error(res.message)
+        }
     };
 
     const normFile = (e: any) => {
@@ -133,6 +197,8 @@ const CreateBook = (props: IProps) => {
                 onOk={() => { form.submit() }}
                 onCancel={() => {
                     form.resetFields();
+                    setFileListSlider([]);
+                    setFileListThumbnail([]);
                     setOpenModalCreate(false);
                 }}
                 destroyOnClose={true}
@@ -217,7 +283,7 @@ const CreateBook = (props: IProps) => {
                                 labelCol={{ span: 24 }}
                                 label="Ảnh Thumbnail"
                                 name="thumbnail"
-                                rules={[{ required: true, message: 'Vui lòng nhập upload thumbnail!' }]}
+                                rules={[{ required: true, message: 'Vui lòng upload thumbnail!' }]}
 
                                 //convert value from Upload => form
                                 valuePropName="fileList"
@@ -228,10 +294,12 @@ const CreateBook = (props: IProps) => {
                                     className="avatar-uploader"
                                     maxCount={1}
                                     multiple={false}
-                                    customRequest={handleUploadFile}
+                                    customRequest={(options) => handleUploadFile(options, 'thumbnail')}
                                     beforeUpload={beforeUpload}
                                     onChange={(info) => handleChange(info, 'thumbnail')}
                                     onPreview={handlePreview}
+                                    onRemove={(file) => handleRemove(file, 'thumbnail')}
+                                    fileList={fileListThumbnail}
                                 >
                                     <div>
                                         {loadingThumbnail ? <LoadingOutlined /> : <PlusOutlined />}
@@ -246,7 +314,7 @@ const CreateBook = (props: IProps) => {
                                 labelCol={{ span: 24 }}
                                 label="Ảnh Slider"
                                 name="slider"
-                                rules={[{ required: true, message: 'Vui lòng nhập upload slider!' }]}
+                                rules={[{ required: true, message: 'Vui lòng upload slider!' }]}
                                 //convert value from Upload => form
                                 valuePropName="fileList"
                                 getValueFromEvent={normFile}
@@ -255,10 +323,12 @@ const CreateBook = (props: IProps) => {
                                     multiple
                                     listType="picture-card"
                                     className="avatar-uploader"
-                                    customRequest={handleUploadFile}
+                                    customRequest={(options) => handleUploadFile(options, 'slider')}
                                     beforeUpload={beforeUpload}
                                     onChange={(info) => handleChange(info, 'slider')}
                                     onPreview={handlePreview}
+                                    onRemove={(file) => handleRemove(file, 'slider')}
+                                    fileList={fileListSlider}
                                 >
                                     <div>
                                         {loadingSlider ? <LoadingOutlined /> : <PlusOutlined />}
--- src/components/admin/book/table.book.tsx
+++ src/components/admin/book/table.book.tsx
@@ -1,30 +1,27 @@
-import { getBooksAPI } from '@/services/api';
-import { dateRangeValidate } from '@/services/helper';
-import { DeleteTwoTone, EditTwoTone, ExportOutlined, PlusOutlined } from '@ant-design/icons';
-import type { ActionType, ProColumns } from '@ant-design/pro-components';
-import { ProTable } from '@ant-design/pro-components';
-import { Button, Popconfirm } from 'antd';
+
 import { useRef, useState } from 'react';
+import { Popconfirm, Button } from 'antd';
+import { DeleteTwoTone, EditTwoTone, ExportOutlined, PlusOutlined } from '@ant-design/icons';
+import { CSVLink } from 'react-csv';
+import { ProTable } from '@ant-design/pro-components';
+import type { ActionType, ProColumns } from '@ant-design/pro-components';
+import { dateRangeValidate } from '@/services/helper';
+import { getBooksAPI } from '@/services/api';
 import DetailBook from './detail.book';
 import CreateBook from './create.book';
 
+
+
 type TSearch = {
     mainText: string;
-    price: string;
     author: string;
-    category: string;
     createdAt: string;
     createdAtRange: string;
+    updatedAt: string;
+    updatedAtRange: string;
+    price: number;
 }
-
 const TableBook = () => {
-
-    // detail
-    const [openViewDetail, setOpenViewDetail] = useState<boolean>(false);
-    const [dataViewDetail, setDataViewDetail] = useState<IBookTable | null>(null);
-
-    //create
-    const [openModalCreate, setOpenModalCreate] = useState<boolean>(false);
 
     const actionRef = useRef<ActionType>();
     const [meta, setMeta] = useState({
@@ -34,6 +31,39 @@ const TableBook = () => {
         total: 0
     });
 
+    const [openViewDetail, setOpenViewDetail] = useState<boolean>(false);
+    const [dataViewDetail, setDataViewDetail] = useState<IBookTable | null>(null);
+
+    const [openModalCreate, setOpenModalCreate] = useState<boolean>(false);
+
+    const [currentDataTable, setCurrentDataTable] = useState<IBookTable[]>([]);
+
+    // const [openModalUpdate, setOpenModalUpdate] = useState<boolean>(false);
+    // const [dataUpdate, setDataUpdate] = useState<IBookTable | null>(null);
+
+    // const [isDeleteBook, setIsDeleteBook] = useState<boolean>(false);
+    // const { message, notification } = App.useApp();
+
+
+    // const handleDeleteBook = async (_id: string) => {
+    //     setIsDeleteBook(true)
+    // const res = await deleteUserAPI(_id);
+    // if (res && res.data) {
+    //     message.success('Xóa book thành công');
+    //     refreshTable();
+    // } else {
+    //     notification.error({
+    //         message: 'Đã có lỗi xảy ra',
+    //         description: res.message
+    //     })
+    // }
+    //     setIsDeleteBook(false)
+    // }
+
+    const refreshTable = () => {
+        actionRef.current?.reload();
+    }
+
     const columns: ProColumns<IBookTable>[] = [
         {
             title: 'Id',
@@ -41,26 +71,28 @@ const TableBook = () => {
             hideInSearch: true,
             render(dom, entity) {
                 return (
-                    <a
-                        onClick={() => {
-                            setDataViewDetail(entity);
-                            setOpenViewDetail(true);
-                        }}
-                        href='#'>{entity._id}</a>
+                    <a href='#' onClick={() => {
+                        setDataViewDetail(entity);
+                        setOpenViewDetail(true);
+                    }}>{entity._id}</a>
                 )
             },
+
         },
         {
-            title: 'Tên Sách',
+            title: 'Tên sách',
             dataIndex: 'mainText',
+            sorter: true
         },
         {
-            title: 'Thể Loại',
+            title: 'Thể loại',
             dataIndex: 'category',
+            hideInSearch: true,
         },
         {
-            title: 'Tác Giả',
+            title: 'Tác giả',
             dataIndex: 'author',
+            sorter: true,
         },
         {
             title: 'Giá tiền',
@@ -77,12 +109,12 @@ const TableBook = () => {
                 )
             }
         },
-
         {
-            title: 'Created At',
-            dataIndex: 'createdAtRange',
-            valueType: 'dateRange',
-            hideInTable: true,
+            title: 'Ngày cập nhật',
+            dataIndex: 'updatedAt',
+            sorter: true,
+            valueType: 'date',
+            hideInSearch: true
         },
 
         {
@@ -92,38 +124,34 @@ const TableBook = () => {
                 return (
                     <>
                         <EditTwoTone
-                            twoToneColor="#f57800"
-                            style={{ cursor: "pointer", marginRight: 15 }}
+                            twoToneColor="#f57800" style={{ cursor: "pointer", margin: "0 5px" }}
                             onClick={() => {
+                                // setOpenModalUpdate(true);
+                                // setDataUpdate(entity);
                             }}
                         />
+
                         <Popconfirm
                             placement="leftTop"
-                            title={"Xác nhận xóa user"}
-                            description={"Bạn có chắc chắn muốn xóa user này ?"}
-                            onConfirm={() => alert(entity._id)}
+                            title={"Xác nhận xóa book"}
+                            description={"Bạn có chắc chắn muốn xóa book này ?"}
+                            // onConfirm={() => handleDeleteBook(entity._id)}
                             okText="Xác nhận"
                             cancelText="Hủy"
-                        // okButtonProps={{ loading: isDeleteUser }}
+                        // okButtonProps={{ loading: isDeleteBook }}
                         >
-                            <span style={{ cursor: "pointer", marginLeft: 20 }}>
-                                <DeleteTwoTone
-                                    twoToneColor="#ff4d4f"
-                                    style={{ cursor: "pointer" }}
-                                />
+                            <span style={{ cursor: "pointer" }}>
+                                <DeleteTwoTone twoToneColor="#ff4d4f" />
                             </span>
                         </Popconfirm>
+
+
                     </>
 
                 )
             }
         }
-
     ];
-
-    // const refreshTable = () => {
-    //     actionRef.current?.reload();
-    // }
 
     return (
         <>
@@ -131,34 +159,44 @@ const TableBook = () => {
                 columns={columns}
                 actionRef={actionRef}
                 cardBordered
-                request={async (params, sort, filter) => {
-                    console.log(params, sort, filter);
+                request={async (params, sort) => {
                     let query = "";
                     if (params) {
                         query += `current=${params.current}&pageSize=${params.pageSize}`
                         if (params.mainText) {
                             query += `&mainText=/${params.mainText}/i`
                         }
-                        if (params.category) {
-                            query += `&category=/${params.category}/i`
+                        if (params.author) {
+                            query += `&author=/${params.author}/i`
                         }
 
                         const createDateRange = dateRangeValidate(params.createdAtRange);
                         if (createDateRange) {
                             query += `&createdAt>=${createDateRange[0]}&createdAt<=${createDateRange[1]}`
                         }
+
                     }
 
-                    //default
 
                     if (sort && sort.createdAt) {
                         query += `&sort=${sort.createdAt === "ascend" ? "createdAt" : "-createdAt"}`
                     } else query += `&sort=-createdAt`;
 
+                    if (sort && sort.mainText) {
+                        query += `&sort=${sort.mainText === "ascend" ? "mainText" : "-mainText"}`
+                    }
+
+                    if (sort && sort.author) {
+                        query += `&sort=${sort.author === "ascend" ? "author" : "-author"}`
+                    }
+                    if (sort && sort.price) {
+                        query += `&sort=${sort.price === "ascend" ? "price" : "-price"}`
+                    }
+
                     const res = await getBooksAPI(query);
                     if (res.data) {
                         setMeta(res.data.meta);
-                        // setCurrentDataTable(res.data?.result ?? [])
+                        setCurrentDataTable(res.data?.result ?? [])
                     }
                     return {
                         data: res.data?.result,
@@ -181,17 +219,16 @@ const TableBook = () => {
 
                 headerTitle="Table book"
                 toolBarRender={() => [
-
                     <Button
                         icon={<ExportOutlined />}
                         type="primary"
                     >
-                        {/* <CSVLink
-                        data={currentDataTable}
-                        filename='export-user.csv'
-                    > */}
-                        Export
-                        {/* </CSVLink> */}
+                        <CSVLink
+                            data={currentDataTable}
+                            filename='export-book.csv'
+                        >
+                            Export
+                        </CSVLink>
                     </Button>,
 
                     <Button
@@ -204,21 +241,24 @@ const TableBook = () => {
                     >
                         Add new
                     </Button>
-
                 ]}
             />
+
             <DetailBook
                 openViewDetail={openViewDetail}
                 setOpenViewDetail={setOpenViewDetail}
                 dataViewDetail={dataViewDetail}
                 setDataViewDetail={setDataViewDetail}
             />
+
             <CreateBook
                 openModalCreate={openModalCreate}
                 setOpenModalCreate={setOpenModalCreate}
+                refreshTable={refreshTable}
             />
         </>
-    );
-};
+    )
+}
+
 
 export default TableBook;
