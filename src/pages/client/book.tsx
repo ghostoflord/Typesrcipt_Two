41df5f04189445f95865eea168db43e3cc09da08
ghostoflord <ghost2892003zzz@gmail.com>
2024-12-28 16:08:13 +0700

    Loading Skeleton

--- src/pages/client/book.tsx
+++ src/pages/client/book.tsx
@@ -1,5 +1,6 @@
 
 import BookDetail from "@/components/client/book/book.detail";
+import BookLoader from "@/components/client/book/book.loader";
 import { getBookByIdAPI } from "@/services/api";
 import { App } from "antd";
 import { useEffect, useState } from "react";
@@ -9,10 +10,12 @@ const BookPage = () => {
     let { id } = useParams();
     const { notification } = App.useApp();
     const [currentBook, setCurrentBook] = useState<IBookTable | null>(null);
+    const [isLoadingBook, setIsLoadingBook] = useState<boolean>(true);
 
     useEffect(() => {
         if (id) {
             const fetchBookById = async () => {
+                setIsLoadingBook(true);
                 const res = await getBookByIdAPI(id);
                 if (res && res.data) {
                     setCurrentBook(res.data);
@@ -22,15 +25,20 @@ const BookPage = () => {
                         description: res.message
                     })
                 }
+                setIsLoadingBook(false);
             }
             fetchBookById();
         }
     }, [id])
     return (
         <div>
-            <BookDetail
-                currentBook={currentBook}
-            />
+            {isLoadingBook ?
+                <BookLoader />
+                :
+                <BookDetail
+                    currentBook={currentBook}
+                />
+            }
         </div>
     )
 }
