ca2d42a6234c3235b12510a304a10d62e840f5ab
ghostoflord <ghost2892003zzz@gmail.com>
2024-12-28 16:05:00 +0700

    update Design View Detail Page

--- src/pages/client/book.tsx
+++ src/pages/client/book.tsx
@@ -1,8 +1,38 @@
+
+import BookDetail from "@/components/client/book/book.detail";
+import { getBookByIdAPI } from "@/services/api";
+import { App } from "antd";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const BookPage = () => {
+    let { id } = useParams();
+    const { notification } = App.useApp();
+    const [currentBook, setCurrentBook] = useState<IBookTable | null>(null);
+
+    useEffect(() => {
+        if (id) {
+            const fetchBookById = async () => {
+                const res = await getBookByIdAPI(id);
+                if (res && res.data) {
+                    setCurrentBook(res.data);
+                } else {
+                    notification.error({
+                        message: 'Đã có lỗi xảy ra',
+                        description: res.message
+                    })
+                }
+            }
+            fetchBookById();
+        }
+    }, [id])
     return (
         <div>
-            BookPage
+            <BookDetail
+                currentBook={currentBook}
+            />
         </div>
     )
 }
+
 export default BookPage;
