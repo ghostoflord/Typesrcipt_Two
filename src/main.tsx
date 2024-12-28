fa093d0e47b49c73ce42317adbcfea64b285d299
ghostoflord <ghost2892003zzz@gmail.com>
2024-12-28 21:12:45 +0700

    update Checkout Page

--- src/main.tsx
+++ src/main.tsx
@@ -41,7 +41,11 @@ const router = createBrowserRouter([
       },
       {
         path: "/order",
-        element: <OrderPage />
+        element: (
+          <ProtectedRoute>
+            <OrderPage />
+          </ProtectedRoute>
+        )
       },
       {
         path: "/about",
