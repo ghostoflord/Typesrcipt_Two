39fe35dc39965dfee0276039a5f37f5e4afe9a5f
ghostoflord <ghost2892003zzz@gmail.com>
2024-12-28 16:59:00 +0700

    update

--- src/components/client/book/book.detail.tsx
+++ src/components/client/book/book.detail.tsx
@@ -1,10 +1,12 @@
-import { Row, Col, Rate, Divider } from 'antd';
+
+import { Row, Col, Rate, Divider, App } from 'antd';
 import ImageGallery from 'react-image-gallery';
 import { useEffect, useRef, useState } from 'react';
 import { MinusOutlined, PlusOutlined } from '@ant-design/icons';
 import { BsCartPlus } from 'react-icons/bs';
 import 'styles/book.scss';
 import ModalGallery from './modal.gallery';
+import { useCurrentApp } from '@/components/context/app.context';
 
 interface IProps {
     currentBook: IBookTable | null;
@@ -26,6 +28,9 @@ const BookDetail = (props: IProps) => {
 
     const refGallery = useRef<ImageGallery>(null);
     const [currentQuantity, setCurrentQuantity] = useState<number>(1);
+
+    const { setCarts } = useCurrentApp();
+    const { message } = App.useApp();
 
     // const images = [
     //     {
@@ -139,6 +144,44 @@ const BookDetail = (props: IProps) => {
         }
     }
 
+    const handleAddToCart = () => {
+        //update localStorage
+        const cartStorage = localStorage.getItem("carts");
+        if (cartStorage && currentBook) {
+            //update
+            const carts = JSON.parse(cartStorage) as ICart[];
+
+            //check exist
+            let isExistIndex = carts.findIndex(c => c._id === currentBook?._id);
+            if (isExistIndex > -1) {
+                carts[isExistIndex].quantity =
+                    carts[isExistIndex].quantity + currentQuantity;
+            } else {
+                carts.push({
+                    quantity: currentQuantity,
+                    _id: currentBook._id,
+                    detail: currentBook
+                })
+            }
+
+            localStorage.setItem("carts", JSON.stringify(carts));
+
+            //sync React Context
+            setCarts(carts);
+        } else {
+            //create
+            const data = [{
+                _id: currentBook?._id!,
+                quantity: currentQuantity,
+                detail: currentBook!
+            }]
+            localStorage.setItem("carts", JSON.stringify(data))
+
+            //sync React Context
+            setCarts(data);
+        }
+        message.success("Thêm sản phẩm vào giỏ hàng thành công.")
+    }
 
     return (
         <div style={{ background: '#efefef', padding: "20px 0" }}>
@@ -198,7 +241,7 @@ const BookDetail = (props: IProps) => {
                                     </span>
                                 </div>
                                 <div className='buy'>
-                                    <button className='cart'>
+                                    <button className='cart' onClick={() => handleAddToCart()}>
                                         <BsCartPlus className='icon-cart' />
                                         <span>Thêm vào giỏ hàng</span>
                                     </button>
