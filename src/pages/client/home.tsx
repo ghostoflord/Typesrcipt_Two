dc284c81bc963147bcc80be4b452ed24851eb117
ghostoflord <ghost2892003zzz@gmail.com>
2024-12-28 15:19:18 +0700

    update filter and sorter homepage

--- src/pages/client/home.tsx
+++ src/pages/client/home.tsx
@@ -4,10 +4,11 @@ import type { FormProps } from 'antd';
 import 'styles/home.scss';
 
 type FieldType = {
-    fullName: string;
-    password: string;
-    email: string;
-    phone: string;
+    range: {
+        from: number;
+        to: number
+    }
+    category: string[]
 };
 
 
@@ -17,10 +18,28 @@ const HomePage = () => {
 
     const handleChangeFilter = (changedValues: any, values: any) => {
         console.log(">>> check handleChangeFilter", changedValues, values)
+        //only fire if category changes
+        if (changedValues.category) {
+            const cate = values.category;
+            if (cate && cate.length > 0) {
+                const f = cate.join(',');
+                setFilter(`category=${f}`)
+            } else {
+                //reset data -> fetch all
+                setFilter('');
+            }
+        }
     }
 
     const onFinish: FormProps<FieldType>['onFinish'] = async (values) => {
-
+        if (values?.range?.from >= 0 && values?.range?.to >= 0) {
+            let f = `price>=${values?.range?.from}&price<=${values?.range?.to}`;
+            if (values?.category?.length) {
+                const cate = values?.category?.join(',');
+                f += `&category=${cate}`
+            }
+            setFilter(f);
+        }
     }
 
     const onChange = (key: string) => {
@@ -29,22 +48,22 @@ const HomePage = () => {
 
     const items = [
         {
-            key: '1',
+            key: "sort=-sold",
             label: `Phổ biến`,
             children: <></>,
         },
         {
-            key: '2',
+            key: 'sort=-updatedAt',
             label: `Hàng Mới`,
             children: <></>,
         },
         {
-            key: '3',
+            key: 'sort=price',
             label: `Giá Thấp Đến Cao`,
             children: <></>,
         },
         {
-            key: '4',
+            key: 'sort=-price',
             label: `Giá Cao Đến Thấp`,
             children: <></>,
         },
@@ -56,7 +75,11 @@ const HomePage = () => {
                 <Col md={4} sm={0} xs={0} style={{ border: "1px solid green" }}>
                     <div style={{ display: 'flex', justifyContent: "space-between" }}>
                         <span> <FilterTwoTone /> Bộ lọc tìm kiếm</span>
-                        <ReloadOutlined title="Reset" onClick={() => form.resetFields()} />
+                        <ReloadOutlined title="Reset" onClick={() => {
+                            form.resetFields();
+                            setFilter('');
+                        }}
+                        />
                     </div>
                     <Form
                         onFinish={onFinish}
--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ export const loginAPI = (username: string, password: string) => {
     const urlBackend = "/api/v1/auth/login";
     return axios.post<IBackendRes<ILogin>>(urlBackend, { username, password }, {
         headers: {
-            delay: 3000
+            delay: 1000
         }
     })
 }
